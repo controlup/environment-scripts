2085926533d4111f357ceb7c87d7a2bf89fb01e6
Guy Leech <guyrleech@gmail.com>
2021-08-24 15:00:42 +0100

    Citrix Cloud added

--- Environment_Synchronization_Scripts/Store credentials.ps1
+++ Environment_Synchronization_Scripts/Store credentials.ps1
@@ -22,6 +22,7 @@
                             12/11/2020 - Guy Leech - added credential type argument for use with Horizon Cloud so one user can have multiple credentials
                             08/12/2020 - Added parameter sets with option for PSCredential object passing (pass as $null to prompt for credentials)
                             07/06/2021 - Merged Azure credentials script
+                            20/08/2021 - Added API client for Citrix Cloud
 
     Changelog ;
         Second Version
@@ -40,10 +41,29 @@
     
     .PARAMETER credentialType
     The type of the credential
+    
+    .PARAMETER credential
+    A PScredential object whose contents will be used to set the relevant properties of the credential file depending on -credentialType
+
+    .PARAMETER tenantId
+    The Azure tenant id for AVD
+
+    .PARAMETER applicationId
+    The Azure tenant application id for AVD
+
+    .PARAMETER applicationSecret
+    The Azure tenant application secret for AVD
+
+    .PARAMETER clientID
+    The Citrix Cloud API client id
+
+    .PARAMETER clientSecret
+    The Citrix Cloud API client secret for the specified API client id
 
     .LINK
     https://code.vmware.com/web/tool/11.3.0/vmware-powercli
     https://github.com/vmware/PowerCLI-Example-Scripts/tree/master/Modules/VMware.Hv.Helper
+    https://us.cloud.com/identity/api-access/secure-clients
 #>
 
 [CmdletBinding(DefaultParameterSetName='ClearText')]
@@ -51,22 +71,32 @@
 Param
 (
     [Parameter(Mandatory,HelpMessage='EUC environment to create credential file for')]
-    [ValidateSet('HorizonView','Azure','HorizonCloudmyVMware','HorizonCloudDomain')]
+    [ValidateSet('HorizonView','Azure','HorizonCloudmyVMware','HorizonCloudDomain','CitrixCloud')]
     [string]$credentialType ,
+
     [Parameter(Mandatory,ParameterSetName='ClearText',HelpMessage='username to store in credential file - email or domain format')]
     [string]$userName ,
     [Parameter(Mandatory,ParameterSetName='ClearText',HelpMessage='Password')]
     [string]$password ,
     [Parameter(Mandatory,ParameterSetName='ClearText',HelpMessage='Password repeated')]
     [string]$passwordAgain ,
+
     [Parameter(Mandatory,ParameterSetName='Credential',HelpMessage='PSCredential object')]
     [System.Management.Automation.PSCredential]$credential ,
+
     [Parameter(Mandatory,ParameterSetName='Azure',HelpMessage='Service Principal Tenant Id')]
     [string]$tenantId ,
     [Parameter(Mandatory,ParameterSetName='Azure',HelpMessage='Service Principal Application (client) Id')]
     [string]$applicationId ,
     [Parameter(Mandatory,ParameterSetName='Azure',HelpMessage='Service Principal Application (client) secret')]
-    [string]$applicationSecret
+    [string]$applicationSecret ,
+
+    [Parameter(Mandatory=$false,ParameterSetName='CitrixCloud', HelpMessage='Citrix Cloud API client id' )]
+    [ValidateNotNullOrEmpty()]
+    [guid]$clientID,
+    [Parameter(Mandatory=$false,ParameterSetName='CitrixCloud', HelpMessage='Citrix Cloud API client secret' )]
+    [ValidateNotNullOrEmpty()]
+    [string]$clientSecret
 )
 
 $VerbosePreference = $(if( $PSBoundParameters[ 'verbose' ] ) { $VerbosePreference } else { 'SilentlyContinue' })
@@ -231,6 +261,14 @@ If ( $credentialType -match 'Azure' ) {
 ElseIf( $PsCmdlet.ParameterSetName -eq 'Credential' )
 {
     New-CUStoredCredential -Username $credential.userName -Password $credential.GetNetworkCredential().Password -System $credentialType
+}
+ElseIf ( $credentialType -match 'CitrixCloud' ) {
+    if( $PsCmdlet.ParameterSetName -eq 'CitrixCloud' ) {
+        New-CUStoredCredential -Username $clientId -Password $clientSecret -System $credentialType
+    }
+    else {
+        Out-CUConsole -Message "Wrong parameters used for $credentialType credential type - use -clientId annd -clientSecret" -Stop
+    }
 }
 ElseIf (!([string]::IsNullOrWhiteSpace( $userName )) -and !([string]::IsNullOrWhiteSpace( $password )) -and $password -eq $passwordAgain ) {
     New-CUStoredCredential -Username $userName -Password $password -System $credentialType
